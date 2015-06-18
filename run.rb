945bb0b8f2b8082fdbd741a5a595496e8d8c7397
Rob Burrowes <r.burrowes@auckland.ac.nz>
2015-06-18 14:18:38 +1200

    Bug fixes in commented out calls
    Left off @ on config in commented out calls. Fixed, incase we uncomment
    them

--- run.rb
+++ run.rb
@@ -9,17 +9,18 @@ require_relative 'generators/gen_wiki_old.rb'
 require_relative 'generators/gen_yml.rb'
 require_relative 'generators/gen_slurm.rb' #Slurm topology file
 
-@config = Configuration.new
+config_file = ARGV[0] == 'test' ? 'conf/local_config.json' : 'conf/config.json'
+@config = Configuration.new config_file
 #@auth = Configuration.new((@config.auth[0] == '/') ? @config.auth : File.expand_path(File.dirname(__FILE__)) + @config.auth)
 begin
-  Dir.chdir(config.base_directory)
+  Dir.chdir(@config.base_directory)
 
   #iblinkinfo = IBLinkInfo.new('iblinkinfo.txt') #Get iblinkinfo from a file
-  iblinkinfo = IBLinkInfo.new(config.iblinkinfo_cmd, true)  #Get iblinkinfo by executing the iblinkinfo command.
+  iblinkinfo = IBLinkInfo.new(@config.iblinkinfo_cmd, true)  #Get iblinkinfo by executing the iblinkinfo command.
 
   #TSV::gen(iblinkinfo, 'iblinkinfo.tsv') #generate a tab separated file, we could load into excel
   #Excel_XML::gen(iblinkinfo, 'iblinkinfo.xml') #Generate an XML file, to be read by Excel (adds formating that we can't have in tsv)
-  #DOT::gen(iblinkinfo, '/tmp/iblinkinfo.dot', config.dot_cmd) #Generate a dot file for input into Grophviz 
+  #DOT::gen(iblinkinfo, '/tmp/iblinkinfo.dot', @config.dot_cmd) #Generate a dot file for input into Grophviz 
   #WIKI::gen(iblinkinfo, 'iblinkinfo_wiki.txt') #Generate Confluence Wiki tables for the switches, then manually add.
   #WIKI_Old::gen(iblinkinfo', 'iblinkinfo_wiki_old.txt') #Old confluence wiki table format.
   #YML::gen(iblinkinfo, 'iblinkinfo.yml') #Generate a YML file from the IBlinkinfo output
